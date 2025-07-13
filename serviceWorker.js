6313d668dd1ca57e82c160ada5f9427720324c1e
Ilia Dubovik <askdubovik@gmail.com>
2025-07-13 19:03:58 +0200

    feat(sw): add html, js, css, png to cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,39 +1,71 @@
 // version 0.0.1
 
+const CACHE_NAME = 'v0';
+
 function putInCache(req, res) {
-  caches.open('v0').then((cache) => {
-    return cache.put(req, res);
-  });
+  return caches.open(CACHE_NAME).then((cache) => cache.put(req, res));
 }
 
-function checkCache(event) {
-  const { request } = event;
+function checkCache(request, event) {
   return caches.match(request).then((cachedData) => {
     if (cachedData) return cachedData;
 
-    return fetch(request)
-      .then((data) => {
-        event.waitUntil(putInCache(request, data.clone()));
-        return data;
-      })
-      .catch((err) => {
-        console.warn(err);
-        return new Response('Network error happened', {
-          status: 408,
-          headers: {
-            'Content-Type': 'text/plain',
-          },
-        });
-      });
+    return fetchData(request, event);
   });
+}
+
+function fetchData(request, event) {
+  return fetch(request)
+    .then((data) => {
+      event.waitUntil(
+        putInCache(request, data.clone()).catch((e) =>
+          console.warn('Failed o put in cache:', e)
+        )
+      );
+      return data;
+    })
+    .catch((err) => {
+      console.warn('Fetch failed:', err);
+      return new Response('Network error happened', {
+        status: 408,
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      });
+    });
 }
 
 self.addEventListener('install', (event) =>
   console.info('Service worker installed')
 );
+
 self.addEventListener('activate', (event) => {
   console.info('Service worker activated');
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.allSettled(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then((result) => console.info('Cache cleanup result:', result))
+  );
 });
+
 self.addEventListener('fetch', function (event) {
-  if (event.request.url.endsWith('.webp')) event.respondWith(checkCache(event));
+  const url = event.request.url;
+  const shouldBeCached =
+    url.endsWith('.webp') ||
+    url.endsWith('.png') ||
+    url.endsWith('.css') ||
+    url.endsWith('.html') ||
+    url.endsWith('.js');
+  const response = shouldBeCached
+    ? checkCache(event.request, event)
+    : fetchData(event.request, event);
+
+  event.respondWith(response);
 });
