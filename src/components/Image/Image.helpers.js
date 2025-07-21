f9cc4b4315c5092cd84e4d7efdc1c52b5bcd4f5f
Ilia Dubovik <askdubovik@gmail.com>
2025-07-21 21:10:27 +0200

    feat(images): [WIP] add new images

--- src/components/Image/Image.helpers.js
+++ src/components/Image/Image.helpers.js
@@ -758,6 +758,1098 @@ export const IMAGES = {
     gerName: 'das Yin und Yang',
     fileName: 'yin-yang.webp',
   },
+  MAILBOX: {
+    gerName: '',
+    fileName: 'mailbox.webp',
+  },
+  SUNSCREEN: {
+    gerName: '',
+    fileName: 'sunscreen.webp',
+  },
+  AIRPORT_TERMINAL: {
+    gerName: '',
+    fileName: 'airport-terminal.webp',
+  },
+  APARTMENT_BUILDING: {
+    gerName: '',
+    fileName: 'apartment-building.webp',
+  },
+  ARCHITECT: {
+    gerName: '',
+    fileName: 'architect.webp',
+  },
+  AUDIO_BOOK: {
+    gerName: '',
+    fileName: 'audio-book.webp',
+  },
+  AUTUMN: {
+    gerName: '',
+    fileName: 'autumn.webp',
+  },
+  BACK_YARD: {
+    gerName: '',
+    fileName: 'back-yard.webp',
+  },
+  BACK: {
+    gerName: '',
+    fileName: 'back.webp',
+  },
+  BACKPACK: {
+    gerName: '',
+    fileName: 'backpack.webp',
+  },
+  BADMINTON: {
+    gerName: '',
+    fileName: 'badminton.webp',
+  },
+  BAGGAGE: {
+    gerName: '',
+    fileName: 'baggage.webp',
+  },
+  BAKERY: {
+    gerName: '',
+    fileName: 'bakery.webp',
+  },
+  BALCONY: {
+    gerName: '',
+    fileName: 'balcony.webp',
+  },
+  BAR_2: {
+    gerName: '',
+    fileName: 'bar-2.webp',
+  },
+  BAR: {
+    gerName: '',
+    fileName: 'bar.webp',
+  },
+  BASKETBALL_2: {
+    gerName: '',
+    fileName: 'basketball-2.webp',
+  },
+  BASKETBALL: {
+    gerName: '',
+    fileName: 'basketball.webp',
+  },
+  BATHROOM: {
+    gerName: '',
+    fileName: 'bathroom.webp',
+  },
+  BEACH: {
+    gerName: '',
+    fileName: 'beach.webp',
+  },
+  BEANIE: {
+    gerName: '',
+    fileName: 'beanie.webp',
+  },
+  BEARD: {
+    gerName: '',
+    fileName: 'beard.webp',
+  },
+  BEER_GLASS: {
+    gerName: '',
+    fileName: 'beer-glass.webp',
+  },
+  BIRKIN_BAG: {
+    gerName: '',
+    fileName: 'birkin-bag.webp',
+  },
+  BIRTHDAY_2: {
+    gerName: '',
+    fileName: 'birthday-2.webp',
+  },
+  BIRTHDAY: {
+    gerName: '',
+    fileName: 'birthday.webp',
+  },
+  BLACKBIRD: {
+    gerName: '',
+    fileName: 'blackbird.webp',
+  },
+  BLOUSE: {
+    gerName: '',
+    fileName: 'blouse.webp',
+  },
+  BOOKSTORE: {
+    gerName: '',
+    fileName: 'bookstore.webp',
+  },
+  BOOT: {
+    gerName: '',
+    fileName: 'boot.webp',
+  },
+  BORDER: {
+    gerName: '',
+    fileName: 'border.webp',
+  },
+  BOSS: {
+    gerName: '',
+    fileName: 'boss.webp',
+  },
+  BOTTLE_OF_WATER: {
+    gerName: '',
+    fileName: 'bottle-of-water.webp',
+  },
+  BOWL: {
+    gerName: '',
+    fileName: 'bowl.webp',
+  },
+  BREAD_ROLL: {
+    gerName: '',
+    fileName: 'bread-roll.webp',
+  },
+  BREAD: {
+    gerName: '',
+    fileName: 'bread.webp',
+  },
+  BREAK_ROOM: {
+    gerName: '',
+    fileName: 'break-room.webp',
+  },
+  BREAST: {
+    gerName: '',
+    fileName: 'breast.webp',
+  },
+  BRIDGE: {
+    gerName: '',
+    fileName: 'bridge.webp',
+  },
+  BROTHER: {
+    gerName: '',
+    fileName: 'brother.webp',
+  },
+  BUG_HOTEL: {
+    gerName: '',
+    fileName: 'bug-hotel.webp',
+  },
+  BULLET_TRAIN: {
+    gerName: '',
+    fileName: 'bullet-train.webp',
+  },
+  BUS_STOP: {
+    gerName: '',
+    fileName: 'bus-stop.webp',
+  },
+  BUSINESS_CARD: {
+    gerName: '',
+    fileName: 'business-card.webp',
+  },
+  BUSINESS_PARTNER: {
+    gerName: '',
+    fileName: 'business-partner.webp',
+  },
+  BUSINESS_TRIP: {
+    gerName: '',
+    fileName: 'business-trip.webp',
+  },
+  CAFE: {
+    gerName: '',
+    fileName: 'cafe.webp',
+  },
+  CAKE: {
+    gerName: '',
+    fileName: 'cake.webp',
+  },
+  CALENDAR: {
+    gerName: '',
+    fileName: 'calendar.webp',
+  },
+  CAR_CRASH: {
+    gerName: '',
+    fileName: 'car-crash.webp',
+  },
+  CARNAVAL: {
+    gerName: '',
+    fileName: 'carnaval.webp',
+  },
+  CARPET: {
+    gerName: '',
+    fileName: 'carpet.webp',
+  },
+  CATHEDRAL: {
+    gerName: '',
+    fileName: 'cathedral.webp',
+  },
+  CERTIFICATE: {
+    gerName: '',
+    fileName: 'certificate.webp',
+  },
+  CHEESE: {
+    gerName: '',
+    fileName: 'cheese.webp',
+  },
+  CHESS: {
+    gerName: '',
+    fileName: 'chess.webp',
+  },
+  CHOCOLATE: {
+    gerName: '',
+    fileName: 'chocolate.webp',
+  },
+  CINEMA: {
+    gerName: '',
+    fileName: 'cinema.webp',
+  },
+  CLASSROOM: {
+    gerName: '',
+    fileName: 'classroom.webp',
+  },
+  COAT: {
+    gerName: '',
+    fileName: 'coat.webp',
+  },
+  COLLEAGUE: {
+    gerName: '',
+    fileName: 'colleague.webp',
+  },
+  COLLEGES: {
+    gerName: '',
+    fileName: 'colleges.webp',
+  },
+  CONFERENCE_HALL: {
+    gerName: '',
+    fileName: 'conference-hall.webp',
+  },
+  COPY_MACHINE: {
+    gerName: '',
+    fileName: 'copy-machine.webp',
+  },
+  CORRIDOR: {
+    gerName: '',
+    fileName: 'corridor.webp',
+  },
+  CREDIT_CARD: {
+    gerName: '',
+    fileName: 'credit-card.webp',
+  },
+  CRIMINAL_DINNER: {
+    gerName: '',
+    fileName: 'criminal-dinner.webp',
+  },
+  CROCODILE: {
+    gerName: '',
+    fileName: 'crocodile.webp',
+  },
+  CROISSANT: {
+    gerName: '',
+    fileName: 'croissant.webp',
+  },
+  CROSSWALK: {
+    gerName: '',
+    fileName: 'crosswalk.webp',
+  },
+  CUCUMBER: {
+    gerName: '',
+    fileName: 'cucumber.webp',
+  },
+  CUP_2: {
+    gerName: '',
+    fileName: 'cup-2.webp',
+  },
+  CUP: {
+    gerName: '',
+    fileName: 'cup.webp',
+  },
+  CUSTOMER: {
+    gerName: '',
+    fileName: 'customer.webp',
+  },
+  CUTLERY: {
+    gerName: '',
+    fileName: 'cutlery.webp',
+  },
+  CV: {
+    gerName: '',
+    fileName: 'cv.webp',
+  },
+  DINNER: {
+    gerName: '',
+    fileName: 'dinner.webp',
+  },
+  DISHWASHER: {
+    gerName: '',
+    fileName: 'dishwasher.webp',
+  },
+  DOOR: {
+    gerName: '',
+    fileName: 'door.webp',
+  },
+  DOORBELL: {
+    gerName: '',
+    fileName: 'doorbell.webp',
+  },
+  DRINKS_MARKET: {
+    gerName: '',
+    fileName: 'drinks-market.webp',
+  },
+  DRIVERS_LICENSE: {
+    gerName: '',
+    fileName: 'drivers-license.webp',
+  },
+  DUNE: {
+    gerName: '',
+    fileName: 'dune.webp',
+  },
+  EASTER: {
+    gerName: '',
+    fileName: 'easter.webp',
+  },
+  ELEVATOR: {
+    gerName: '',
+    fileName: 'elevator.webp',
+  },
+  ERASER: {
+    gerName: '',
+    fileName: 'eraser.webp',
+  },
+  EXAM: {
+    gerName: '',
+    fileName: 'exam.webp',
+  },
+  EXHIBITION: {
+    gerName: '',
+    fileName: 'exhibition.webp',
+  },
+  EYE: {
+    gerName: '',
+    fileName: 'eye.webp',
+  },
+  FACE_MASK: {
+    gerName: '',
+    fileName: 'face-mask.webp',
+  },
+  FATHER: {
+    gerName: '',
+    fileName: 'father.webp',
+  },
+  FIELD: {
+    gerName: '',
+    fileName: 'field.webp',
+  },
+  FINGER: {
+    gerName: '',
+    fileName: 'finger.webp',
+  },
+  FIREFIGHTERS_CAR: {
+    gerName: '',
+    fileName: 'firefighters-car.webp',
+  },
+  FITNESS_STUDIO: {
+    gerName: '',
+    fileName: 'fitness-studio.webp',
+  },
+  FLOOR: {
+    gerName: '',
+    fileName: 'floor.webp',
+  },
+  FLOWERS_SHOP: {
+    gerName: '',
+    fileName: 'flowers-shop.webp',
+  },
+  FOOT: {
+    gerName: '',
+    fileName: 'foot.webp',
+  },
+  FORK: {
+    gerName: '',
+    fileName: 'fork.webp',
+  },
+  FOUNTAIN: {
+    gerName: '',
+    fileName: 'fountain.webp',
+  },
+  GARAGE: {
+    gerName: '',
+    fileName: 'garage.webp',
+  },
+  GARDEN: {
+    gerName: '',
+    fileName: 'garden.webp',
+  },
+  GIRAFFE: {
+    gerName: '',
+    fileName: 'giraffe.webp',
+  },
+  GLASS_WATER_JUG: {
+    gerName: '',
+    fileName: 'glass-water-jug.webp',
+  },
+  GLASSES_SHOP: {
+    gerName: '',
+    fileName: 'glasses-shop.webp',
+  },
+  GOLF: {
+    gerName: '',
+    fileName: 'golf.webp',
+  },
+  GORILLA: {
+    gerName: '',
+    fileName: 'gorilla.webp',
+  },
+  GRADUATE: {
+    gerName: '',
+    fileName: 'graduate.webp',
+  },
+  GRANDPA_2: {
+    gerName: '',
+    fileName: 'grandpa-2.webp',
+  },
+  GRANDPARENTS: {
+    gerName: '',
+    fileName: 'grandparents.webp',
+  },
+  GROCERY_BAG: {
+    gerName: '',
+    fileName: 'grocery-bag.webp',
+  },
+  HAIR: {
+    gerName: '',
+    fileName: 'hair.webp',
+  },
+  HAIRSTYLIST: {
+    gerName: '',
+    fileName: 'hairstylist.webp',
+  },
+  HANDYMAN: {
+    gerName: '',
+    fileName: 'handyman.webp',
+  },
+  HANGER_2: {
+    gerName: '',
+    fileName: 'hanger-2.webp',
+  },
+  HANGER: {
+    gerName: '',
+    fileName: 'hanger.webp',
+  },
+  HEALTH_INSURANCE_2: {
+    gerName: '',
+    fileName: 'health-insurance-2.webp',
+  },
+  HIGHWAY: {
+    gerName: '',
+    fileName: 'highway.webp',
+  },
+  HILL: {
+    gerName: '',
+    fileName: 'hill.webp',
+  },
+  HOLIDAY: {
+    gerName: '',
+    fileName: 'holiday.webp',
+  },
+  HOMEWORK: {
+    gerName: '',
+    fileName: 'homework.webp',
+  },
+  HOSPITAL: {
+    gerName: '',
+    fileName: 'hospital.webp',
+  },
+  HOUSE_NUMBER: {
+    gerName: '',
+    fileName: 'house-number.webp',
+  },
+  HOUSE: {
+    gerName: '',
+    fileName: 'house.webp',
+  },
+  HR: {
+    gerName: '',
+    fileName: 'hr.webp',
+  },
+  INJECTION: {
+    gerName: '',
+    fileName: 'injection.webp',
+  },
+  INVOICE: {
+    gerName: '',
+    fileName: 'invoice.webp',
+  },
+  IT_DEPARTMENT: {
+    gerName: '',
+    fileName: 'it-department.webp',
+  },
+  JAM: {
+    gerName: '',
+    fileName: 'jam.webp',
+  },
+  JOB_INTERVIEW: {
+    gerName: '',
+    fileName: 'job-interview.webp',
+  },
+  JUDO: {
+    gerName: '',
+    fileName: 'judo.webp',
+  },
+  JUICE: {
+    gerName: '',
+    fileName: 'juice.webp',
+  },
+  KARAOKE: {
+    gerName: '',
+    fileName: 'karaoke.webp',
+  },
+  KETCHUP: {
+    gerName: '',
+    fileName: 'ketchup.webp',
+  },
+  KEYBOARD: {
+    gerName: '',
+    fileName: 'keyboard.webp',
+  },
+  KINDERGARTEN: {
+    gerName: '',
+    fileName: 'kindergarten.webp',
+  },
+  KITCHEN_STOVE: {
+    gerName: '',
+    fileName: 'kitchen-stove.webp',
+  },
+  KITCHEN: {
+    gerName: '',
+    fileName: 'kitchen.webp',
+  },
+  KIWI: {
+    gerName: '',
+    fileName: 'kiwi.webp',
+  },
+  LAW: {
+    gerName: '',
+    fileName: 'law.webp',
+  },
+  LEMON: {
+    gerName: '',
+    fileName: 'lemon.webp',
+  },
+  LEMONADE: {
+    gerName: '',
+    fileName: 'lemonade.webp',
+  },
+  LIBRARY: {
+    gerName: '',
+    fileName: 'library.webp',
+  },
+  LIGHT: {
+    gerName: '',
+    fileName: 'light.webp',
+  },
+  LION: {
+    gerName: '',
+    fileName: 'lion.webp',
+  },
+  MAGIC_SHOW: {
+    gerName: '',
+    fileName: 'magic-show.webp',
+  },
+  MAKEUP: {
+    gerName: '',
+    fileName: 'makeup.webp',
+  },
+  MARKET: {
+    gerName: '',
+    fileName: 'market.webp',
+  },
+  MEADOW: {
+    gerName: '',
+    fileName: 'meadow.webp',
+  },
+  MEAT: {
+    gerName: '',
+    fileName: 'meat.webp',
+  },
+  MEETING: {
+    gerName: '',
+    fileName: 'meeting.webp',
+  },
+  MELON_PAN: {
+    gerName: '',
+    fileName: 'melon-pan.webp',
+  },
+  MONKEY: {
+    gerName: '',
+    fileName: 'monkey.webp',
+  },
+  MOTHER: {
+    gerName: '',
+    fileName: 'mother.webp',
+  },
+  MUSEUM: {
+    gerName: '',
+    fileName: 'museum.webp',
+  },
+  MUSICAL: {
+    gerName: '',
+    fileName: 'musical.webp',
+  },
+  NETWORK: {
+    gerName: '',
+    fileName: 'network.webp',
+  },
+  NEWSPAPER: {
+    gerName: '',
+    fileName: 'newspaper.webp',
+  },
+  OFFICE_KITCHEN: {
+    gerName: '',
+    fileName: 'office-kitchen.webp',
+  },
+  OIL: {
+    gerName: '',
+    fileName: 'oil.webp',
+  },
+  ONION: {
+    gerName: '',
+    fileName: 'onion.webp',
+  },
+  OPERA_MASK: {
+    gerName: '',
+    fileName: 'opera-mask.webp',
+  },
+  ORANGE: {
+    gerName: '',
+    fileName: 'orange.webp',
+  },
+  PAINTING_NAILS: {
+    gerName: '',
+    fileName: 'painting-nails.webp',
+  },
+  PAJAMAS: {
+    gerName: '',
+    fileName: 'pajamas.webp',
+  },
+  PARENTS: {
+    gerName: '',
+    fileName: 'parents.webp',
+  },
+  PARROT: {
+    gerName: '',
+    fileName: 'parrot.webp',
+  },
+  PASSPORT: {
+    gerName: '',
+    fileName: 'passport.webp',
+  },
+  PATCH: {
+    gerName: '',
+    fileName: 'patch.webp',
+  },
+  PEPPER: {
+    gerName: '',
+    fileName: 'pepper.webp',
+  },
+  PHARMACY: {
+    gerName: '',
+    fileName: 'pharmacy.webp',
+  },
+  PHONE_CHARGER: {
+    gerName: '',
+    fileName: 'phone-charger.webp',
+  },
+  PHONE_MARKET: {
+    gerName: '',
+    fileName: 'phone-market.webp',
+  },
+  PICTURE: {
+    gerName: '',
+    fileName: 'picture.webp',
+  },
+  PIE: {
+    gerName: '',
+    fileName: 'pie.webp',
+  },
+  PINEAPPLE: {
+    gerName: '',
+    fileName: 'pineapple.webp',
+  },
+  PIZZA: {
+    gerName: '',
+    fileName: 'pizza.webp',
+  },
+  PLASTIC_BAG: {
+    gerName: '',
+    fileName: 'plastic-bag.webp',
+  },
+  PLATE: {
+    gerName: '',
+    fileName: 'plate.webp',
+  },
+  PLAYGROUND: {
+    gerName: '',
+    fileName: 'playground.webp',
+  },
+  PODCAST: {
+    gerName: '',
+    fileName: 'podcast.webp',
+  },
+  POLICE_CAR: {
+    gerName: '',
+    fileName: 'police-car.webp',
+  },
+  POPSICLE: {
+    gerName: '',
+    fileName: 'popsicle.webp',
+  },
+  POST_OFFICE: {
+    gerName: '',
+    fileName: 'post-office.webp',
+  },
+  POT: {
+    gerName: '',
+    fileName: 'pot.webp',
+  },
+  POTATO: {
+    gerName: '',
+    fileName: 'potato.webp',
+  },
+  POWERBANK: {
+    gerName: '',
+    fileName: 'powerbank.webp',
+  },
+  PRESENTATION: {
+    gerName: '',
+    fileName: 'presentation.webp',
+  },
+  PRINTER: {
+    gerName: '',
+    fileName: 'printer.webp',
+  },
+  PRODUCTION: {
+    gerName: '',
+    fileName: 'production.webp',
+  },
+  PROJECT: {
+    gerName: '',
+    fileName: 'project.webp',
+  },
+  PROTEST: {
+    gerName: '',
+    fileName: 'protest.webp',
+  },
+  RADIATOR: {
+    gerName: '',
+    fileName: 'radiator.webp',
+  },
+  RAILWAY_STATION: {
+    gerName: '',
+    fileName: 'railway-station.webp',
+  },
+  RAIN: {
+    gerName: '',
+    fileName: 'rain.webp',
+  },
+  RAW_FISH: {
+    gerName: '',
+    fileName: 'raw-fish.webp',
+  },
+  READING: {
+    gerName: '',
+    fileName: 'reading.webp',
+  },
+  RECEPTION: {
+    gerName: '',
+    fileName: 'reception.webp',
+  },
+  RECYCLING: {
+    gerName: '',
+    fileName: 'recycling.webp',
+  },
+  RICE: {
+    gerName: '',
+    fileName: 'rice.webp',
+  },
+  RIVER: {
+    gerName: '',
+    fileName: 'river.webp',
+  },
+  ROCK_CLIMBING: {
+    gerName: '',
+    fileName: 'rock-climbing.webp',
+  },
+  ROOF: {
+    gerName: '',
+    fileName: 'roof.webp',
+  },
+  ROOM: {
+    gerName: '',
+    fileName: 'room.webp',
+  },
+  ROWING: {
+    gerName: '',
+    fileName: 'rowing.webp',
+  },
+  SALAD: {
+    gerName: '',
+    fileName: 'salad.webp',
+  },
+  SANDWICH: {
+    gerName: '',
+    fileName: 'sandwich.webp',
+  },
+  SCREEN: {
+    gerName: '',
+    fileName: 'screen.webp',
+  },
+  SCUBA_DIVING: {
+    gerName: '',
+    fileName: 'scuba-diving.webp',
+  },
+  SECRETARY: {
+    gerName: '',
+    fileName: 'secretary.webp',
+  },
+  SHIRT: {
+    gerName: '',
+    fileName: 'shirt.webp',
+  },
+  SHOPPING_MALL: {
+    gerName: '',
+    fileName: 'shopping-mall.webp',
+  },
+  SIBLINGS: {
+    gerName: '',
+    fileName: 'siblings.webp',
+  },
+  SISTER: {
+    gerName: '',
+    fileName: 'sister.webp',
+  },
+  SKETCHBOOK: {
+    gerName: '',
+    fileName: 'sketchbook.webp',
+  },
+  SLEEP: {
+    gerName: '',
+    fileName: 'sleep.webp',
+  },
+  SLEEPING_BAG: {
+    gerName: '',
+    fileName: 'sleeping-bag.webp',
+  },
+  SNOW: {
+    gerName: '',
+    fileName: 'snow.webp',
+  },
+  SNOWMAN: {
+    gerName: '',
+    fileName: 'snowman.webp',
+  },
+  SOCKET: {
+    gerName: '',
+    fileName: 'socket.webp',
+  },
+  SOCKS: {
+    gerName: '',
+    fileName: 'socks.webp',
+  },
+  SODA: {
+    gerName: '',
+    fileName: 'soda.webp',
+  },
+  SOUP: {
+    gerName: '',
+    fileName: 'soup.webp',
+  },
+  SOUVENIR: {
+    gerName: '',
+    fileName: 'souvenir.webp',
+  },
+  SPOON: {
+    gerName: '',
+    fileName: 'spoon.webp',
+  },
+  SPRING: {
+    gerName: '',
+    fileName: 'spring.webp',
+  },
+  STAIR: {
+    gerName: '',
+    fileName: 'stair.webp',
+  },
+  STOMACH: {
+    gerName: '',
+    fileName: 'stomach.webp',
+  },
+  STRAWBERRY: {
+    gerName: '',
+    fileName: 'strawberry.webp',
+  },
+  STREET: {
+    gerName: '',
+    fileName: 'street.webp',
+  },
+  STUDENT_ID: {
+    gerName: '',
+    fileName: 'student-id.webp',
+  },
+  SUBWAY: {
+    gerName: '',
+    fileName: 'subway.webp',
+  },
+  SUGAR: {
+    gerName: '',
+    fileName: 'sugar.webp',
+  },
+  SUIT: {
+    gerName: '',
+    fileName: 'suit.webp',
+  },
+  SUITCASE: {
+    gerName: '',
+    fileName: 'suitcase.webp',
+  },
+  SUMMER: {
+    gerName: '',
+    fileName: 'summer.webp',
+  },
+  SUN: {
+    gerName: '',
+    fileName: 'sun.webp',
+  },
+  SWIMMING_POOL: {
+    gerName: '',
+    fileName: 'swimming-pool.webp',
+  },
+  T_SHIRTS: {
+    gerName: '',
+    fileName: 't-shirts.webp',
+  },
+  TALKING: {
+    gerName: '',
+    fileName: 'talking.webp',
+  },
+  TEA: {
+    gerName: '',
+    fileName: 'tea.webp',
+  },
+  TEACHER: {
+    gerName: '',
+    fileName: 'teacher.webp',
+  },
+  THEATER: {
+    gerName: '',
+    fileName: 'theater.webp',
+  },
+  TOMATO: {
+    gerName: '',
+    fileName: 'tomato.webp',
+  },
+  TOOTH: {
+    gerName: '',
+    fileName: 'tooth.webp',
+  },
+  TOOTHPASTE: {
+    gerName: '',
+    fileName: 'toothpaste.webp',
+  },
+  TORTOISE: {
+    gerName: '',
+    fileName: 'tortoise.webp',
+  },
+  TOWN_SQUARE: {
+    gerName: '',
+    fileName: 'town-square.webp',
+  },
+  TRAFFIC_LIGHT_2: {
+    gerName: '',
+    fileName: 'traffic-light-2.webp',
+  },
+  TRAIN_PLATFORM: {
+    gerName: '',
+    fileName: 'train-platform.webp',
+  },
+  TRAINEE: {
+    gerName: '',
+    fileName: 'trainee.webp',
+  },
+  TRASH: {
+    gerName: '',
+    fileName: 'trash.webp',
+  },
+  TREE: {
+    gerName: '',
+    fileName: 'tree.webp',
+  },
+  TV: {
+    gerName: '',
+    fileName: 'tv.webp',
+  },
+  VACATION: {
+    gerName: '',
+    fileName: 'vacation.webp',
+  },
+  VALLEY: {
+    gerName: '',
+    fileName: 'valley.webp',
+  },
+  VIDEOGAME_2: {
+    gerName: '',
+    fileName: 'videogame-2.webp',
+  },
+  VILLAGE: {
+    gerName: '',
+    fileName: 'village.webp',
+  },
+  WAITRESS: {
+    gerName: '',
+    fileName: 'waitress.webp',
+  },
+  WALK: {
+    gerName: '',
+    fileName: 'walk.webp',
+  },
+  WARDROBE: {
+    gerName: '',
+    fileName: 'wardrobe.webp',
+  },
+  WATER_BOTTLE: {
+    gerName: '',
+    fileName: 'water-bottle.webp',
+  },
+  WATER_GLASS: {
+    gerName: '',
+    fileName: 'water-glass.webp',
+  },
+  WHEELCHAIR: {
+    gerName: '',
+    fileName: 'wheelchair.webp',
+  },
+  WIND: {
+    gerName: '',
+    fileName: 'wind.webp',
+  },
+  WINDOW: {
+    gerName: '',
+    fileName: 'window.webp',
+  },
+  WINTER: {
+    gerName: '',
+    fileName: 'winter.webp',
+  },
+  WORK_DINNING: {
+    gerName: '',
+    fileName: 'work-dinning.webp',
+  },
+  WORK: {
+    gerName: '',
+    fileName: 'work.webp',
+  },
+  WORLD: {
+    gerName: '',
+    fileName: 'world.webp',
+  },
+  YOGA: {
+    gerName: '',
+    fileName: 'yoga.webp',
+  },
+  ZOO: {
+    gerName: '',
+    fileName: 'zoo.webp',
+  },
+  СHRISTMAS: {
+    gerName: '',
+    fileName: 'сhristmas.webp',
+  },
 };
 
 export const IMAGE_NAMES_ARR = Object.keys(IMAGES);
